acc45395e912f4bd375ab7774f3ce201ff48eccd
Akazawa Kazairo <akazawakazairo@gmail.com>
2025-08-09 22:07:04 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,92 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
+/*
+	* Create By Naze
+	* Follow https://github.com/nazedev
+	* Whatsapp : https://whatsapp.com/channel/0029VaWOkNm7DAWtkvkJBK43
+*/
+
+//~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
+
+global.owner = ['6285762427949','6283142478779'] //['628','628'] 2 owner atau lebih
+global.author = 'ArdiCeline'
+global.botname = 'Arcel-Bot'
+global.packname = 'Arcel Development'
+global.listprefix = ['+','!','.']
+
+/*
+- Setting Author, Packname, Botname sekarang pakai command
+- Walau sudah diganti tidak berubah
+- setbotauthor, setbotpackname, setbotname
+*/
+
+global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
+global.tempatDB = 'database.json' // Taruh url mongodb di sini jika menggunakan mongodb. Format : 'mongodb+srv://...'
+global.tempatStore = 'baileys_store.json' // Taruh url mongodb di sini jika menggunakan mongodb. Format : 'mongodb+srv://...'
+global.pairing_code = true
+global.number_bot = '' // Kalo pake panel bisa masukin nomer di sini, jika belum ambil session. Format : '628xx'
+
+global.fake = {
+	anonim: 'https://telegra.ph/file/95670d63378f7f4210f03.png',
+	thumbnailUrl: 'https://telegra.ph/file/fe4843a1261fc414542c4.jpg',
+	thumbnail: fs.readFileSync('./src/media/naze.png'),
+	docs: fs.readFileSync('./src/media/fake.pdf'),
+	listfakedocs: ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','application/vnd.openxmlformats-officedocument.presentationml.presentation','application/vnd.openxmlformats-officedocument.wordprocessingml.document','application/pdf'],
+}
+
+global.my = {
+	yt: 'https://youtube.com/c/AkazawaKazairo',
+	gh: 'https://github.com/AkazawaKazairo',
+	gc: 'https://chat.whatsapp.com/BbICDrNyuBtGP1BlNVzQuV',
+	ch: '12036325040961@newsletter',
+}
+
+global.limit = {
+	free: 200,
+	premium: 9999,
+	vip: 99999
+}
+
+global.money = {
+	free: 100000,
+	premium: 10000000,
+	vip: 100000000
+}
+
+global.mess = {
+	key: 'Apikey mu udah habis nih, silahkan hubungi owner',
+	owner: 'Fitur buat owner doang jink!',
+	admin: 'Jadi admin dulu gih baru pake fitur ini',
+	botAdmin: 'jadiin bot admin dulu kocak',
+	group: 'pake di grup lah anjink',
+	private: 'pake di chat pribadi ama bot aja',
+	limit: 'Limitmu abis loh anjay',
+	prem: 'cuman buat orang yang premium doang loh yaa',
+	wait: 'Wait yak, loading nih...',
+	error: 'anjink error cok',
+	done: 'Kelar'
+}
+
+global.APIs = {
+	hitori: 'https://api.hitori.pw',
+}
+global.APIKeys = {
+	'https://api.hitori.pw': 'htrkey-77eb83c0eeb39d40',
+	geminiApikey: ['AIzaSyD0lkGz6ZhKi_MHSSmJcCX3wXoDZhELPaQ','AIzaSyDnBPd_EhBfr73NssnThVQZYiKZVhGZewU','AIzaSyA94OZD-0V4quRbzPb2j75AuzSblPHE75M','AIzaSyB5aTYbUg2VQ0oXr5hdJPN8AyLJcmM84-A','AIzaSyB1xYZ2YImnBdi2Bh-If_8lj6rvSkabqlA']
+}
+
+// Lainnya
+
+global.badWords = ['tolol','goblok','asu','pantek','kampret','ngentot','jancok','kontol','memek','lonte']
+global.chatLength = 1000
+
+//~~~~~~~~~~~~~~~< PROCESS >~~~~~~~~~~~~~~~\\
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+});
